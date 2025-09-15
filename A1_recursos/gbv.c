176ceceb7a17ab5c46857e3f23cb8a6ae25ed3bd
Raphael <gaias.raphael@ufpr.br>
2025-09-15 18:06:53 -0300

    estudar arquivos

--- A1_recursos/gbv.c
+++ A1_recursos/gbv.c
@@ -3,10 +3,22 @@
 #include "gbv.h"
 
 
-int gbv_create(const char *filename);
-int gbv_open(Library *lib, const char *filename);
+int gbv_create(const char *filename){
+    if (!filename) return 0;
+
+
+}
+int gbv_open(Library *lib, const char *filename){
+    if (!lib || !filename) return 1;
+    FILE *file = fopen ("filename", "rb");
+    file.
+    lib->docs
+}
 int gbv_add(Library *lib, const char *archive, const char *docname);
 int gbv_remove(Library *lib, const char *docname);
-int gbv_list(const Library *lib);
+
+int gbv_list(const Library *lib){
+    if (!lib) return 0;
+}
+
 int gbv_view(const Library *lib, const char *docname);
-int gbv_order(Library *lib, const char *archive, const char *criteria);
