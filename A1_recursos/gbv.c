dd18ab79bdfd2d865f4b93ffab99e1d66a3d9397
Raphael <gaias.raphael@ufpr.br>
2025-09-17 18:58:21 -0300

    terminar create

--- A1_recursos/gbv.c
+++ A1_recursos/gbv.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "gbv.h"
+#include "util.h"
 
-
+// função chamada se arquivo não existe
 int gbv_create(const char *filename){
-    if (!filename) return 0;
+    if (!filename) return 1;
+    
+    FILE *file = fopen (filename, "w+b");
+    if (!file){
+        perror ("Erro ao abrir arquivo\n");
+        exit (1);
+    }
 
+    struct bloco *b = cria_bloco ();
+    if (!b) return 1;
+
+    fwrite (b, sizeof (struct bloco), 1, file);
 
 }
+
 int gbv_open(Library *lib, const char *filename){
-    if (!lib || !filename) return 1;
-    FILE *file = fopen ("filename", "rb");
-    file.
-    lib->docs
+    if (!filename) return 1;
+
+    FILE *file = fopen (filename, "r+b");
+
+    if (!file){
+        gbv_create (filename);
+        file = fopen (filename, "r+b");
+    }
+    
+    gbv_create (lib);
 }
 int gbv_add(Library *lib, const char *archive, const char *docname);
 int gbv_remove(Library *lib, const char *docname);
--- A1_recursos/util.h
+++ A1_recursos/util.h
@@ -6,5 +6,12 @@
 // Converte time_t para string formatada
 void format_date(time_t t, char *buffer, int max);
 
+struct bloco {
+    long num_arquivos;
+    long *offset;
+};
+
+struct bloco *cria_bloco ();
+
 #endif
 
